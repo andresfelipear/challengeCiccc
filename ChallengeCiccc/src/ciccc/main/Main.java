1cb099c79d31b9602ea1edfad8b777e2db2c27a3
Andres Arevalo <andresfelipear@gmail.com>
2021-06-19 12:57:34 -0700

    testing the function stringToBinary

--- ChallengeCiccc/src/ciccc/main/Main.java
+++ ChallengeCiccc/src/ciccc/main/Main.java
@@ -1,4 +1,16 @@
 package ciccc.main;
 
+import ciccc.functions.Functions;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        Functions functions = new Functions();
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter your email: ");
+        String email = console.next();
+        String emailBinary = functions.stringToBinary(email);
+        System.out.println("Your email " + email + " to binary is "+ emailBinary);
+    }
 }
