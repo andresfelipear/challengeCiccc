32ec57ab442eb9072949becda1636a1b782e38f3
Andres Arevalo <andresfelipear@gmail.com>
2021-06-19 23:17:29 -0700

    understanding better the ceaser cipher and how works the rotation

--- ChallengeCiccc/src/ciccc/functions/Functions.java
+++ ChallengeCiccc/src/ciccc/functions/Functions.java
@@ -21,17 +21,23 @@ public class Functions {
 
     public static String caesarCipher(String str, int rotation){
         char[] chars = str.toCharArray();
-        final int  STARTALPHA = 97;
+        final int  STARTALPHA = 96;
+        if(rotation >26){
+            rotation = rotation%26;
+        }
+        System.out.println(rotation);
         StringBuilder encripter = new StringBuilder();
         for(char ch:chars){
+            System.out.println(rotation);
             int aux = ch;
             int ascii=0;
+            System.out.println(aux);
             if(aux > 122 || aux<STARTALPHA){
                 ascii=aux;
             }
             else if((aux+rotation)>122){
-                int dif = (aux+rotation)-122;
-                ascii=STARTALPHA+dif;
+                int dif = (aux + rotation) - 122;
+                ascii = STARTALPHA + dif;
             }
             else{
                 ascii=aux+rotation;
